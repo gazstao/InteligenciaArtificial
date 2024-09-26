83ef17fa2ee0d91ae28839549b44f7c6af68dd2c
gazstao <gazstao@gmail.com>
2024-09-25 22:54:04 -0300

    v 2.03

--- Python/youtubeResume.py
+++ Python/youtubeResume.py
@@ -1,35 +1,45 @@
-import requests
+import json
 import ollama
 from youtube_transcript_api import YouTubeTranscriptApi
 import re
+import webbrowser
+import os
 
-ollama.list()
+
+# Ajuste o modelo, se necessário
+modelo = "phi3.5:latest"  
+prompt = "Faça um resumo do seguinte texto, pontuando os itens principais e descrevendo o mais detalhadamente possivel seu conteudo: "
+idioma = 'pt'
+
+
 # Função para extrair a transcrição do YouTube
-def get_transcript(video_id, language='pt'):
+def get_transcript(video_url, language=idioma):
+
+    # Obtem somente o ID do video
+    video_id = video_url.split("v=")[-1]
+
     try:
+        # Obtem a transcrição do video escolhido na linguagem pre-definida, une o texto fragmentado e retorna o resultado
         transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[language])
         transcript_text = " ".join([entry['text'] for entry in transcript])
         return transcript_text
     except Exception as e:
-        print(f"Erro ao obter a transcrição: {e}")
+        print(f"Erro ao obter a transcrição na função get_transcript: {e}")
         return None
 
-# Função para obter um resumo usando o Ollama API
-def summarize_with_ollama(text):
 
-    modelo = "llama3.1:latest"  # Ajuste o modelo, se necessário
-    prompt = "Faça um resumo conciso do seguinte texto: "+text
+# Função para obter um resumo usando o Ollama
+def summarize_with_ollama(text, arquivoID):
 
     try:
-        saida = ollama.chat(
-            model=modelo, 
-            messages=[{'role':'user','content': prompt}]
-            )
+        saida = ollama.chat(model=modelo, messages=[{'role':'user','content': prompt+text}])
+        json_bonito_saida = json.dumps(saida,indent=4)
+        print(json_bonito_saida)
 
-        print(saida)
-        with open("resumos.txt","a") as arquivo:
+        with open(".\\data\\"+arquivoID+".txt","a") as arquivo:
             arquivo.write(saida['message']['content'])
-            arquivo.write("\n\n\n\n----------------------------------------\n\n\n\n")
+            arquivo.write("\n\n----------------------------------------\n\n")
+        return(saida['message']['content'])
     except Exception as e:
         print(f"Erro: {e}")
         return None
@@ -37,29 +47,74 @@ def summarize_with_ollama(text):
 # Função principal
 def main():
     video_url = input("Insira o URL do vídeo do YouTube: ")
+    transcript = get_transcript(video_url)
 
-    # Extrai o ID do vídeo a partir da URL
-    video_id = video_url.split("v=")[-1]
-
-    # Obter a transcrição
-    transcript = get_transcript(video_id)
     if transcript:
         print("Transcrição obtida com sucesso...\nAnalisando o resultado. Aguarde...")
         
         # Enviar a transcrição para o Ollama e obter o resumo
-        summary = summarize_with_ollama(filtrar_string(transcript))
+        summary = summarize_with_ollama(filtrar_string(transcript),video_url)
         if summary:
-            print("\nResumo gerado:")
+            print("\nResumo gerado: ")
             print(summary)
+            criaHtml(summary, video_url.split("v=")[-1])
         else:
             print("Falha ao gerar o resumo.")
     else:
         print("Não foi possível obter a transcrição.")
+    
+
 
 def filtrar_string(texto):
     # Remove caracteres que não são letras, números ou espaços
     texto_filtrado = re.sub(r'[^\w\s]', '', texto)
     return texto_filtrado
+
+
+def criaHtml(texto, video_id):
+    conteudo_html = f"""
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>GazsTao YouTube Resumer</title>
+        <style>
+            body {{
+                font-family: Arial, sans-serif;
+                margin: 20px;
+                background-color: #f4f4f4;
+            }}
+
+        pre {{
+            padding: 15px;
+            text-align: justify;
+            color: #abc;
+            border: 2px solid #568ea6;
+            margin: auto;
+            white-space: pre-wrap;
+            word-wrap: break-word; 
+            border-radius: 7px;
+        }}   
+        </style>
+    </head>
+    <body>
+        <h1>GazsTao YouTubeResumer</h1>
+        <pre>{texto}</pre>
+    </body>
+    </html>
+    """
+
+    # Nome do arquivo HTML
+    arquivo_html = '\\data\\'+video_id+'.html'
+
+    # Salvando o arquivo HTML
+    with open(arquivo_html, 'w', encoding='utf-8') as f:
+        f.write(conteudo_html)
+
+    # Abrindo o arquivo HTML no navegador automaticamente
+    caminho_absoluto = os.path.abspath(arquivo_html)
+    webbrowser.open(f'file://{caminho_absoluto}')
 
 if __name__ == "__main__":
     main()
